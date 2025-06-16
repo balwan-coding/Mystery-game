1d32abc7103ada20634406fbdf9aa4f42b1805c4
balwan coding <balwansinghrajput1708@gmail.com>
2025-06-16 09:05:49 +0530

    add setting function

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -1,37 +1,21 @@
 import FristIntro from "./FristIntro";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { RiRadioButtonLine } from "react-icons/ri";
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LevelOne from "./LevelOne";
 
 const Home = () => {
   const [color, setColor] = useState(false);
+  const [isOn, setIsOn] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
   const handleChange = () => {
     setColor(!color);
     setIsVisible(!isVisible);
+    setIsOn(!isOn);
   };
-
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  // ✅ हर बार जब route बदले, उसे localStorage में save करो
-  useEffect(() => {
-    localStorage.setItem("lastPath", location.pathname + location.search);
-  }, [location]);
-
-  // ✅ अगर user "/" पर आया है और कोई lastPath stored है, तो redirect कर दो
-  useEffect(() => {
-    const lastPath = localStorage.getItem("lastPath");
-
-    if (location.pathname === "/" && lastPath && lastPath !== "/") {
-      navigate(lastPath, { replace: true });
-    }
-  }, []);
-
   return (
     <>
-      <div className="border-50 rounded-4xl border-gray-600 mt-0 mr-0 ml-0 mb-20  bg-black relative h-screen">
+      <div className="border-50 border-gray-600 mt-0 mr-0 ml-0 mb-0  bg-black relative h-screen">
         <Routes>
           <Route index element={<FristIntro isVisible={isVisible} />}></Route>
           <Route
@@ -42,7 +26,9 @@ const Home = () => {
 
         <button
           onClick={handleChange}
-          className="text-white text-2xl absolute -bottom-[34px] right-40 cursor-pointer"
+          className={` ${
+            isOn ? "text-green-500" : "text-white"
+          } text-2xl absolute -bottom-[34px] right-40 cursor-pointer duration-300`}
         >
           <RiRadioButtonLine />
         </button>
@@ -50,7 +36,7 @@ const Home = () => {
         <span
           className={` ${
             color ? "bg-red-500" : "bg-white"
-          } p-2 border rounded-full absolute shadow-blue-900 -bottom-[30px] right-28 duration-300 ease-in`}
+          } p-2 border rounded-full absolute shadow-blue-900 shadow-2xs -bottom-[30px] right-28 duration-300 ease-in`}
         ></span>
       </div>
     </>
--- src/componets/LevelOne.jsx
+++ src/componets/LevelOne.jsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function LevelOne({ isVisible }) {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleClick = (e) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    setPosition({ x, y });
+  };
   return (
     <div
       className={`h-full
              bg-green-700 ${
                isVisible ? "visible" : "hidden"
-             } flex items-center justify-center flex-col gap-5`}
+             } flex  flex-col gap-1`}
     >
-      <h2>Welcome to level one</h2>
-      <Link
-        to="/"
-        className="text-white flex gap-3 rounded-lg items-center justify-center bg-yellow-500 p-3 text-2xl hover:text-black hover:bg-yellow-400"
-      >
-        Go to home
-      </Link>
+      <div>
+        <h2 className="text-yellow-500 mt-2 ml-2 text-2xl font-semibold ">
+          Welcome to level one
+        </h2>
+        <Link
+          to="/"
+          className="text-white self-start flex mt-2 ml-2 gap-3 rounded-lg items-center justify-center bg-yellow-500 p-3 text-2xl hover:text-black hover:bg-yellow-400"
+        >
+          Go to home
+        </Link>
+      </div>
+
+      <div className="bg-indigo-900 h-full relevite" onClick={handleClick}>
+        <div
+          className="bg-white h-1 w-7 absolute "
+          style={{ left: position.x, top: position.y }}
+        ></div>
+      </div>
     </div>
   );
 }
